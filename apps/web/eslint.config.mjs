a2b6015379ab63c450b78b0bd41c3d3b0e3a83b2
ThanhVu <vutienthanh0210@gmail.com>
2026-01-13 17:37:40 +0700

    lint

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,10 +1,22 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
